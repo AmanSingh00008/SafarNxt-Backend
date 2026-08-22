49f3580e747490bf6f7fff63dff4eb2951c2b55c
Anurag <anuragkumar002004@gmail.com>
2026-08-22 21:55:12 +0530

    Initialize Express server and set up basic listening functionality and create a usermodel

---
+++ app.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => {
+    console.log(`server is listening on port 3000`)
+})
+
+export {app}
+
