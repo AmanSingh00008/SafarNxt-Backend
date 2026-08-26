49158d467bb62e6dd17c644b5e5001005810cbc8
Anurag <anuragkumar002004@gmail.com>
2026-08-26 23:25:43 +0530

    feat: add JWT token generation methods to user model and fix schema formatting

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,46 +1,72 @@
-import mongoose from "mongoose"
-
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
 
 const userSchema = new Schema({
-    username: {
-        type: String,
-        required: true,
-        trim: true,
-        index: true,
-        unique: true
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+    index: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    trim: true,
+    required: true,
+    lowercase: true,
+  },
+  username: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+    unique: true,
+  },
+  fullname: {
+    type: String,
+    required: true,
+    index: true,
+    trim: true,
+  },
+  refreshToken: {
+    type: String,
+  },
+});
+{
+  timestamps: true;
+}
 
+userSchema.methods.generateAccessToken = function () {
+  const token = jwt.sign(
+    {
+      _id: this._id,
+      email: this.email,
+      username: this.username,
+      fullname: this.fullname,
     },
-    email: {
-        type: String,
-        unique: true,
-        trim: true,
-        required: true,
-        lowercase: true
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
     },
-    username: {
-        type: String,
-        required: true,
-        trim: true,
+  );
+  return token;
+};
 
-    },
-    password: {
-        type: String,
-        required: [true, "Password is required"],
-        unique: true,
-
-    },
-    fullname: {
-        type: String,
-        required: true,
-        index: true,
-        trim: true
-
-    },
-    refreshToken: {
-        type: String,
-    }
-})
-{timestamps: true}
-
-
-export const User = mongoose.model("user", userSchema)
+userSchema.methods.genearateRefreshToken() = function (){
+    const token = jwt.sign(
+        {
+            _id: this._id,
+           
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+          expiresIn:  process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+    return token;
+}
+export const User = mongoose.model("user", userSchema);
