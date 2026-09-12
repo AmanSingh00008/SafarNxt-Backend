825119212fd8fd43173551c0a23f54b6f950fc77
Anurag <anuragkumar002004@gmail.com>
2026-09-12 20:31:43 +0530

    feat: add router dependency and update user routes for consistency

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,34 +1,36 @@
-import { Router } from "express"; 
+import { Router } from "express";
 
 import {
-    LoginUser,
-    accessRefreshToken,
-    RegisterUser,
-    LogOutUser,
-    getCurrentUser,
-    changeCurrentPassword,
-    updateAccountDetails
+  LoginUser,
+  accessRefreshToken,
+  RegisterUser,
+  LogOutUser,
+  getCurrentUser,
+  changeCurrentPassword,
+  updateAccountDetails,
+} from "../controllers/user.controllers.js";
 
-} from "../controllers/user.controllers.js"
-
-import {upload} from "../middlewares/multer.middleware.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
-router.route ( "/Register").post(
-    upload.fields([
-        { name: "avatar", maxcount: 1},
-        {
-            name: "coverImage",
-            maxcount: 1
-        },
-    ]),
-    registerUser
-)
+router.route("/Register").post(
+  upload.fields([
+    { name: "avatar", maxcount: 1 },
+    {
+      name: "coverImage",
+      maxcount: 1,
+    },
+  ]),
+  RegisterUser,
+);
 
-router.route("/login").post(LoginUser)
+router.route("/login").post(LoginUser);
 router.route("/accessRefreshToken").post(accessRefreshToken);
 
-router.route
+router.route("/LogOutUser").post(LogOutUser);
+router.route("/getCurrentUser").get(getCurrentUser);
+router.route("/changeCurrentPassword").patch(changeCurrentPassword);
+router.route("/updateAccountDetails").patch(updateAccountDetails);
